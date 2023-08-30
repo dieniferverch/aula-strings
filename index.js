172e443c5270d2ee984b6f6b277efb03079ad6ff
dieniferverch <142456047+dieniferverch@users.noreply.github.com>
2023-08-30 20:55:54 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,48 @@
+// DIENIFER VERCH
+
+// 1 -
+// a) undefined
+// b) null
+// c)11
+// d)3
+// e) [3, 19, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+//f) 9 
+
+
+// 2- SUBI NUM ONIBUS EM MIRROCOS, 27
+
+// B . Exercícios de escrita de código
+
+//let nomeDoUsuario = prompt("qual seu nome?");
+//let emailDoUsuario = prompt("qual seu email?");
+//console.log(`O e-mail ${emailDoUsuario} foi cadastrado com sucesso. Seja bem-vinda(o), ${nomeDoUsuario}`);
+
+//let comidasPreferidas = ["pudim", "lasanha", "sushi", "pizza","churrasco"];
+//console.log(comidasPreferidas);
+//console.log("essas são minhas comidas preferidas");
+//console.log(comidasPreferidas[0]);
+//console.log(comidasPreferidas[1]);
+//console.log(comidasPreferidas[2]);
+//console.log(comidasPreferidas[3]);
+//console.log(comidasPreferidas[4]);
+
+//let comidaDoUsuario = prompt("qual sua comida preferida?");
+//comidasPreferidas[1] = comidaDoUsuario;
+//console.log(comidasPreferidas);
+
+//let listaDeTarefas = [];
+//let primeiraTarefa = prompt("escreve uma tarefa que você fez no dia")
+//listaDeTarefas.push(primeiraTarefa);
+
+//let segundaTarefa = prompt("escreve uma tarefa que você fez no dia")
+//listaDeTarefas.push(segundaTarefa);
+
+//let terceiraTarefa = prompt("escreve uma tarefa que você fez no dia")
+//listaDeTarefas.push(terceiraTarefa);
+
+//console.log(listaDeTarefas);
+
+//let tarefaRealizada = prompt(`informe qual tarefa você realizou no dia de hoje?Digite 0 para ${primeiraTarefa}, digite 1 para ${segundaTarefa}, ou 2 para ${terceiraTarefa}`);
+
+//listaDeTarefas.splice(tarefaRealizada, 1);
+//console.log(listaDeTarefas);
